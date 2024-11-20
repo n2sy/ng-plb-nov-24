2d68cb0112dc362138abb45443036d5d84dfa88a
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2024-11-20 11:33:30 +0100

    modify angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FirstComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
