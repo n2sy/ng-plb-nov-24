e31ed952fa394a943b8a7b809596e3a5157c7c38
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2024-11-20 14:36:43 +0100

    first step project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
 import { FormsModule } from '@angular/forms';
 import { ChildComponent } from './child/child.component';
+import { CvComponent } from './cv/cv.component';
+import { ListeComponent } from './liste/liste.component';
+import { DetailsComponent } from './details/details.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
-  declarations: [AppComponent, FirstComponent, ChildComponent],
+  declarations: [AppComponent, FirstComponent, ChildComponent, CvComponent, ListeComponent, DetailsComponent, ItemComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/models/candidat.ts
@@ -0,0 +1,20 @@
+export class Candidat {
+  constructor(
+    public id: number,
+    public prenom: string,
+    public nom: string,
+    public age: number,
+    public profession: string,
+    public avatar?: string
+  ) {}
+
+  // public id : number;
+  // prenom : string;
+  // nom : string;
+
+  // constructor(id : number, prenom : string, nom : string) {
+  //     this.id = id;
+  //     this.prenom = prenom;
+  //     this.nom = nom;
+  // }
+}
