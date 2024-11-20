749691b83b8532d732560b58409f6823eaf36e9f
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2024-11-20 12:02:34 +0100

    4 bindings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, FirstComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -4,4 +4,20 @@ import { Component } from '@angular/core';
   selector: 'app-first',
   templateUrl: './first.component.html',
 })
-export class FirstComponent {}
+export class FirstComponent {
+  prenom: string = 'Pauline';
+  ville = 'Paris';
+
+  color: string = 'pink';
+  cache: boolean = false;
+
+  traitement() {
+    alert("J'ai été cliqué !");
+  }
+
+  modifierVille(nouvelleVille: string) {
+    console.log(nouvelleVille);
+
+    this.ville = nouvelleVille;
+  }
+}
